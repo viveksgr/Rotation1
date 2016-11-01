2a6c6af0fe59b7aa02280b21481ab53636717cb2
viveksgr <VivekSagar2016@u.northwestern.edu>
2016-11-01 13:09:57 -0500

    Siamese Network added.

---
+++ Python_scripts/Misc/dump.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+""" Siamese implementation using Tensorflow with MNIST example.
+This siamese network embeds a 28x28 image (a point in 784D) 
+into a point in 2D.
+
+By Youngwook Paul Kwon (young at berkeley.edu)
+"""
+
+    def loss_with_spring(self):
+        margin = 5.0
+        labels_t = self.y_
+        labels_f = tf.sub(1.0, self.y_, name="1-yi")          # labels_ = !labels;
+        eucd2 = tf.pow(tf.sub(self.o1, self.o2), 2)
+        eucd2 = tf.reduce_sum(eucd2, 1)
+        eucd = tf.sqrt(eucd2+1e-6, name="eucd")
+        C = tf.constant(margin, name="C")
+        # yi*||CNN(p1i)-CNN(p2i)||^2 + (1-yi)*max(0, C-||CNN(p1i)-CNN(p2i)||^2)
+        pos = tf.mul(labels_t, eucd2, name="yi_x_eucd2")
+        # neg = tf.mul(labels_f, tf.sub(0.0,eucd2), name="yi_x_eucd2")
+        # neg = tf.mul(labels_f, tf.maximum(0.0, tf.sub(C,eucd2)), name="Nyi_x_C-eucd_xx_2")
+        neg = tf.mul(labels_f, tf.pow(tf.maximum(tf.sub(C, eucd), 0), 2), name="Nyi_x_C-eucd_xx_2")
+        losses = tf.add(pos, neg, name="losses")
+        loss = tf.reduce_mean(losses, name="loss")
+        return loss
+
+    def loss_with_step(self):
+        margin = 5.0
+        labels_t = self.y_
+        labels_f = tf.sub(1.0, self.y_, name="1-yi")          # labels_ = !labels;
+        eucd2 = tf.pow(tf.sub(self.o1, self.o2), 2)
+        eucd2 = tf.reduce_sum(eucd2, 1)
+        eucd = tf.sqrt(eucd2+1e-6, name="eucd")
+        C = tf.constant(margin, name="C")
+        pos = tf.mul(labels_t, eucd, name="y_x_eucd")
+        neg = tf.mul(labels_f, tf.maximum(0.0, tf.sub(C, eucd)), name="Ny_C-eucd")
+        losses = tf.add(pos, neg, name="losses")
+        loss = tf.reduce_mean(losses, name="loss")
+        return loss
+        
+
+        
+        
